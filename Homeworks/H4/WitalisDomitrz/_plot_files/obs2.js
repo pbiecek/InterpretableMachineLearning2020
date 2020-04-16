8d082144b5eafcfd718b7b8bc62ec7107d4899cd
wdomitrz <witekdomitrz@gmail.com>
2020-04-16 18:11:27 +0200

    Fourth homework, ipynb only

---
+++ Homeworks/H4/WitalisDomitrz/_plot_files/obs2.js
@@ -0,0 +1,17 @@
+observation = [
+  {
+    "Pclass": 1.0,
+    "Age": 63.0,
+    "SibSp": 1.0,
+    "Parch": 0.0,
+    "Fare": 221.77920532226562,
+    "Sex_male": 0.0,
+    "Embarked_C": 0.0,
+    "Embarked_Q": 0.0,
+    "Embarked_S": 1.0,
+    "_yhat_": 0.9588261842727661,
+    "_label_": "XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,\n              colsample_bynode=1, colsample_bytree=1, gamma=0, gpu_id=-1,\n              importance_type='gain', interaction_constraints=None,\n              learning_rate=0.1, max_delta_step=0, max_depth=4,\n              min_child_weight=1, missing=nan, monotone_constraints=None,\n              n_estimators=100, n_jobs=1, nthread=1, num_parallel_tree=1,\n              objective='binary:logistic', random_state=0, reg_alpha=0,\n              reg_lambda=1, scale_pos_weight=1, seed=0, silent=None,\n              subsample=1, tree_method=None, validate_parameters=False,\n              verbosity=1)",
+    "_ids_": 0,
+    "_y_": 1.0
+  }
+];
